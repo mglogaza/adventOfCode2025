f4b1735519fc63e145aa95ed980e500b39771ff3
mglogaza <mglogaza@gmail.com>
2025-12-03 23:09:42 +0100

    day 3

--- src/main/java/org/example/Day3.java
+++ src/main/java/org/example/Day3.java
@@ -1,4 +1,62 @@
 package org.example;
 
+import java.io.IOException;
+
+import static java.lang.Character.getNumericValue;
+
 public class Day3 {
+    public static void main(String[] args) throws IOException {
+        part1();
+        part2();
+    }
+
+    private static void part1() throws IOException {
+        try (var reader = Util.getInput("/day3.txt")) {
+            String line;
+            int sum = 0;
+            while ((line = reader.readLine()) != null) {
+                int max = 0, firstMaxIndex = 0;
+                var characters = line.toCharArray();
+                for (int i = 0; i < characters.length - 1; i++) {
+                    if (getNumericValue(characters[i]) > max) {
+                        firstMaxIndex = i;
+                        max = getNumericValue(characters[i]);
+                    }
+                }
+                int secondDigit = 0;
+                for (int i = firstMaxIndex + 1; i < characters.length; i++) {
+                    int numericValue = getNumericValue(characters[i]);
+                    if (numericValue > secondDigit) {
+                        secondDigit = numericValue;
+                    }
+                }
+                sum += max * 10 + secondDigit;
+            }
+            System.out.println("Part 1: " + sum);
+        }
+    }
+
+    private static void part2() throws IOException {
+        try (var reader = Util.getInput("/day3.txt")) {
+            String line;
+            long sum = 0;
+            while ((line = reader.readLine()) != null) {
+                int max = 0, maxIndex = 0;
+                for (int d = 11; d >= 0; d--) {
+                    var characters = line.toCharArray();
+                    for (int i = maxIndex; i < characters.length - d; i++) {
+                        if (getNumericValue(characters[i]) > max) {
+                            maxIndex = i;
+                            max = getNumericValue(characters[i]);
+                        }
+                    }
+                    sum += (long) (Math.pow(10, d) * max);
+                    maxIndex++;
+                    max = 0;
+                }
+            }
+            System.out.println("Part 2: " + sum);
+        }
+    }
+
 }
--- src/main/java/org/example/Util.java
+++ src/main/java/org/example/Util.java
@@ -1,4 +1,11 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Util {
+    public static BufferedReader getInput(String name) {
+        return new BufferedReader(new InputStreamReader(Util.class.getResourceAsStream(name)));
+    }
+
 }
